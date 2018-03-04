d3be323d9cf83e8506bb9e110bb606776bd7184c
asbsoft <ab2014box@gmail.com>
2018-03-04 20:11:18 +0200

    parameters descriptions files

---
+++ messages/en/params.php
@@ -0,0 +1,18 @@
+<?php
+//en
+return [
+    'useExternalStartPage'
+ => "if TRUE then use external start page from application defaultRoute,"
+  . " if FALSE then as start page will use first content page with root = 0",
+
+    'canAuthorEditOwnVisibleArticle' => "Indicate when article published (is_visible = true) author can't edit it",
+
+    'maxImageSize'   => 'maximum image size in bytes',
+    'pageSizeAdmin'  => 'admin list page size',
+    'slugMinLength'  => 'minimum required length of slug',
+    'titleMinLength' => 'minimum required length of title',
+    'textMinLength'  => "minimum required length of articles' text in symbols",
+    'editAllLanguages' => 'set TRUE to show in edit form all registered languages, not only visible',
+    'showAdminSqlErrors' => 'show in backend detail SQL-errors diagnostic if appear',
+
+];
